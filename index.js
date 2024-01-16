65aa805fe90a3403ff0a80eec4485c7cccff21c2
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2024-01-16 15:34:41 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, str) {
+  let greetings = [] 
+  for(let name of names) {
+    greetings.push(`Thank you, ${name}, for the wonderful ${str} gift!`)
+  }
+  return greetings
+}
+
+function countDown(num) {
+  while(num > -1) {
+    console.log(num)
+    num--
+  }
+}
